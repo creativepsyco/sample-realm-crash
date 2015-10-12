2b4ac8295dd55945167eb42d7ecbdbe2a8decdd4
Mohit Kanwal <mohit.kanwal@gmail.com>
2015-10-12 17:02:34 +0800

    Update files for the realm crash during compress call.

--- app/src/main/java/mohitkanwal/com/examplerealm/ContactHelper.java
+++ app/src/main/java/mohitkanwal/com/examplerealm/ContactHelper.java
@@ -1,10 +1,13 @@
 package mohitkanwal.com.examplerealm;
 
+import android.content.Context;
+
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.List;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
@@ -13,10 +16,14 @@ import io.realm.RealmResults;
  * Date: 28/11/14
  */
 public class ContactHelper {
-    private static final int MAX_FIXTURES = 500;
+    private static final int MAX_FIXTURES = 100;
+
+    private static SecureRandom random = new SecureRandom();
+
+    private static RealmConfiguration realmConfiguration;
 
     public static Contact getOrCreateContactModel(Integer id) {
-        Realm realm = Realm.getInstance(ExampleApp.getApplication());
+        Realm realm = getMeARealmFile(ExampleApp.getApplication());
         RealmQuery<Contact> query = realm.where(Contact.class);
         query.equalTo("id", id.longValue());
         Contact theContact = query.findFirst();
@@ -28,27 +35,40 @@ public class ContactHelper {
     }
 
 
+    private static Realm getMeARealmFile(Context application) {
+
+        if (realmConfiguration == null) {
+            realmConfiguration = new RealmConfiguration.Builder(application)
+                    .name("SampleRealmFile")
+                    .schemaVersion(1)
+                    .build();
+
+            // Fails here
+            Realm.compactRealm(realmConfiguration);
+        }
+
+        return Realm.getInstance(realmConfiguration);
+    }
+
     public static Contact getContactById(long id) {
-        Realm realm = Realm.getInstance(ExampleApp.getApplication());
+        Realm realm = getMeARealmFile(ExampleApp.getApplication());
         RealmQuery<Contact> query = realm.where(Contact.class);
         query.equalTo("id", id);
         return query.findFirst();
     }
 
     public static List<Contact> getAllContacts() {
-        RealmQuery<Contact> query = Realm.getInstance(ExampleApp.getApplication())
+        RealmQuery<Contact> query = getMeARealmFile(ExampleApp.getApplication())
                 .where(Contact.class);
-        return query.findAll().sort("firstName", RealmResults.SORT_ORDER_ASCENDING);
+        return query.findAllSorted("firstName", RealmResults.SORT_ORDER_ASCENDING);
     }
 
     public static String getDisplayName(Contact data) {
         return data.getFirstName() + " " + data.getLastName();
     }
 
-    private static SecureRandom random = new SecureRandom();
-
     public static void populate() {
-        Realm realm = Realm.getInstance(ExampleApp.getApplication());
+        Realm realm = getMeARealmFile(ExampleApp.getApplication());
         for (int i = 0; i < MAX_FIXTURES; i++) {
             realm.beginTransaction();
             Contact contact = realm.createObject(Contact.class);
